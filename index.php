dd35e224d839bfb5900196a75fa1697b8b8e638f
Mitchel Leal <lealm@onid.oregonstate.edu>
2015-05-10 19:12:37 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<body>
+?>
+
+<?php
+<form></form>
+?>
+
+function tableFunc();
+
+conditional statements;
+
+function deleteFunc();
+
+function dropDown();
+
+function tableFilter();
+
+</body>
+</html>
+
+
+
